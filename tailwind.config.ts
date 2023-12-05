ee8583a52bc5ba9bc5e67bd51e0b2db824b6f772
GiandomenicoRiceputi <giandomenicoriceputi@gmail.com>
2023-12-05 20:13:45 +0100

    step errors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,75 +1,30 @@
-import {Config} from "tailwindcss";
-
-const themes = {
-  base: {
-    50: '#eef2ff',
-    100: '#e0e7ff',
-    200: '#d0d7f7',
-    300: '#aab9ff',
-    400: '#8994ff',
-    500: '#6b70fc',
-    600: '#554fee',
-    700: '#453ccd',
-    800: '#3830a2',
-    900: '#272173',
-  },
-  rainforest: {
-    50: '#ecfdf4',
-    100: '#c9f2de',
-    200: '#9de9c6',
-    300: '#56d0a0',
-    400: '#00b380',
-    500: '#009268',
-    600: '#007955',
-    700: '#006344',
-    800: '#005038',
-    900: '#003422',
-  },
-  candy: {
-    50: '#fdf2f8',
-    100: '#f7e2ee',
-    200: '#f8cce5',
-    300: '#f5a4d0',
-    400: '#f271b5',
-    500: '#e13d90',
-    600: '#c31667',
-    700: '#a1004b',
-    800: '#84003d',
-    900: '#590028',
-  },
-};
-
-// Extract variables
-const { base, rainforest, candy } = themes;
+import { Config } from "tailwindcss";
+import * as multiThemePlugin from "./multi-theme-plugin";
 
 const config: Config = {
   content: [
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        base,
-        rainforest,
-        candy,
-        // You can add the primary color scheme here if you have one
         primary: {
-          50: 'rgb(var(--primary-50) / <alpha-value>)',
-          100: 'rgb(var(--primary-100) / <alpha-value>)',
-          200: 'rgb(var(--primary-200) / <alpha-value>)',
-          300: 'rgb(var(--primary-300) / <alpha-value>)',
-          400: 'rgb(var(--primary-400) / <alpha-value>)',
-          500: 'rgb(var(--primary-500) / <alpha-value>)',
-          600: 'rgb(var(--primary-600) / <alpha-value>)',
-          700: 'rgb(var(--primary-700) / <alpha-value>)',
-          800: 'rgb(var(--primary-800) / <alpha-value>)',
-          900: 'rgb(var(--primary-900) / <alpha-value>)',
+          50: "rgb(var(--primary-50) / <alpha-value>)",
+          100: "rgb(var(--primary-100) / <alpha-value>)",
+          200: "rgb(var(--primary-200) / <alpha-value>)",
+          300: "rgb(var(--primary-300) / <alpha-value>)",
+          400: "rgb(var(--primary-400) / <alpha-value>)",
+          500: "rgb(var(--primary-500) / <alpha-value>)",
+          600: "rgb(var(--primary-600) / <alpha-value>)",
+          700: "rgb(var(--primary-700) / <alpha-value>)",
+          800: "rgb(var(--primary-800) / <alpha-value>)",
+          900: "rgb(var(--primary-900) / <alpha-value>)",
         },
       },
     },
-    plugins: [],
-  }
+  },
+  plugins: [multiThemePlugin],
 };
 
 export default config;
